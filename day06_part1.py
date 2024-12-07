1f6864898cc49396d27384a32be149cae577ff2e
Rayk Oscuro <wer.auch.immer@gmx.net>
2024-12-07 03:11:19 +0100

    removed unnecessary print()ing in day 6

--- day06_part1.py
+++ day06_part1.py
@@ -13,36 +13,36 @@ with open(sys.argv[1]) as f:
         for j, char in enumerate(x):
             if char == "^":
                 currentPos = [i,j]
-            gridData[i][j] = {"type": "." if char == "^" else char, "visited": True if char == "^" else False}
+            gridData[i][j] = {"type": "." if char == "^" else char, "visited": [0,1] if char == "^" else False}
 
 directions = [[-1,0],[0,1], [1,0], [0,-1]]
 currentDir = 0
 
-print(currentPos)
-print(gridData[currentPos[0]][currentPos[1]]["type"]=='.')
+# print(currentPos)
+# print(gridData[currentPos[0]][currentPos[1]]["type"]=='.')
 
 while True:
     maybePos = [currentPos[0]+directions[currentDir][0],currentPos[1]+directions[currentDir][1]]
     if maybePos[0] < 0 or maybePos[0] >= len(gridData) or maybePos[1] < 0 or maybePos[1] >= len(gridData[0]):
         break
     if gridData[maybePos[0]][maybePos[1]]["type"]=="#":
-        print("boop")
+        # print("boop")
         currentDir+=1
         if currentDir==4:
             currentDir=0
     else:
         currentPos[0]=maybePos[0] 
         currentPos[1]=maybePos[1]
-        print("hello!")
+        # print("hello!")
         gridData[maybePos[0]][maybePos[1]]["visited"]=True
 
-print(gridData)
+# print(gridData)
 
 total=0
 
 for x in gridData:
     for y in x:
-        print(y)
+        # print(y)
         if y["visited"]==True:
             total+=1
             
